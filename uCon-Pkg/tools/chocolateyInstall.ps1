f847bc357d907906018ae22f3ce39584e8749e2b
justindoehling <justin.doehling@gmail.com>
2015-04-02 10:13:24 -0500

    Added uCon package
    http://www.umonfw.com/ucon/index.html

---
+++ uCon-Pkg/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'uCon' 
+$installerType = 'exe'
+$scriptDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+$installFile = "http://www.umonfw.com/releases/ucon_install.exe"
+$silentArgs = "/S"
+$validExitCodes = @(0) 
+
+try {  
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installFile" -validExitCodes $validExitCodes 
+
+} catch { 
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)" 
+  throw 
+
+}
