fe8397031b0ef63f2e89ea47fdfc2dc035b644c9
Justin Doehling <jdoehling@jjkeller.com>
2015-06-12 09:53:02 -0500

    Updated nuspec and install script for ucon

--- uCon-Pkg/tools/chocolateyInstall.ps1
+++ uCon-Pkg/tools/chocolateyInstall.ps1
@@ -1,15 +1,8 @@
-﻿$packageName = 'uCon' 
+﻿$packageName = 'ucon' 
 $installerType = 'exe'
 $scriptDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 $installFile = "http://www.umonfw.com/releases/ucon_install.exe"
 $silentArgs = "/S"
 $validExitCodes = @(0) 
 
-try {  
-  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installFile" -validExitCodes $validExitCodes 
-
-} catch { 
-	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)" 
-  throw 
-
-}
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installFile" -validExitCodes $validExitCodes 
