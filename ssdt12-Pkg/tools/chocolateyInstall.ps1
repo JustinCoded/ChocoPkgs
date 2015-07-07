9f78fa83531e6ff7ec5755f331e0b6761fd5fed9
Justin Doehling <jdoehling@jjkeller.com>
2015-07-07 11:27:19 -0500

    Added ssdt for Visual Studio 2013

---
+++ ssdt12-Pkg/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'ssdt12' 
+$installerType = 'exe'
+$scriptDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+$installFile = "http://go.microsoft.com/fwlink/?LinkID=393521&clcid=0x409"
+$silentArgs = "/passive /norestart"
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installFile" -validExitCodes $validExitCodes 
